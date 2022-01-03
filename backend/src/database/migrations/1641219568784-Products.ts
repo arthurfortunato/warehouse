f95c68482932ef79ebb1dc032d7cc56eed1c559c
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-01-03 11:29:13 -0300

    Criando a migration Products

---
+++ backend/src/database/migrations/1641219568784-Products.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Products1641219568784 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'products',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'code',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'sector',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'price',
+                        type: 'numeric',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('products')
+    }
+
+}
